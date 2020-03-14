62c5db9dacf9a46cbcea40e106f023dad179305b
mingsichen <173498060@qq.com>
2020-03-14 17:39:29 +0800

    猫眼电影大体完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import movieRouter from './movie/movie'
+import cinemaRouter from './cinema/cinema'
+import mineRouter from './mine/mine'
+
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    movieRouter,
+    mineRouter,
+    cinemaRouter,
+    /* 重宝项路由 */
+    {
+      path: '/*',
+      redirect:'/movie'
+    }
+  ]
 })
 
 export default router
